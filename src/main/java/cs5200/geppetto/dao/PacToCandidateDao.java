df4dd78ae4578ced0c0a356cf99429927211d7e1
Joshua Veden <veden.joshua@gmail.com>
2016-11-17 14:35:48 -0800

    added PacToCandidateDao jdbc implementation and PacToCandidateDao interface

--- src/main/java/cs5200/geppetto/dao/PacToCandidateDao.java
+++ src/main/java/cs5200/geppetto/dao/PacToCandidateDao.java
@@ -4,16 +4,16 @@ import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 
-import cs5200.geppetto.model.Candidate;
 import cs5200.geppetto.model.PacToCandidateDonation;
 
 public interface PacToCandidateDao {
   PacToCandidateDonation create(PacToCandidateDonation donation) throws SQLException;
 
-  List<PacToCandidateDonation> getDonationsByCandidate(Candidate candidate) throws SQLException;
+  List<PacToCandidateDonation> getDonationsByCandidateName(String candidateName)
+      throws SQLException;
 
   PacToCandidateDonation updateDonationDate(PacToCandidateDonation donation, Date date)
       throws SQLException;
 
-  PacToCandidateDonation deleteDonation(PacToCandidateDonation donation) throws SQLException;
+  PacToCandidateDonation deleteDonation(String fecRecNo) throws SQLException;
 }
--- src/main/java/cs5200/geppetto/dao/impl/JdbcPacToCandidateDao.java
+++ src/main/java/cs5200/geppetto/dao/impl/JdbcPacToCandidateDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -11,7 +12,6 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import cs5200.geppetto.dao.PacToCandidateDao;
-import cs5200.geppetto.model.Candidate;
 import cs5200.geppetto.model.PacToCandidateDonation;
 
 /**
@@ -24,26 +24,72 @@ public class JdbcPacToCandidateDao extends MyJdbcDaoSupport implements PacToCand
    * {@inheritDoc}
    */
   @Override
-  public PacToCandidateDonation create(PacToCandidateDonation donation) {
-    // TODO Auto-generated method stub
-    return null;
+  public PacToCandidateDonation create(PacToCandidateDonation donation) throws SQLException {
+    String insertDonation = "INSERT INTO"
+        + " PACsToCand16(Cycle,FECRecNo,PACID,CID,Amount,Date,RealCode,Type,DI,FECCandId)"
+        + " VALUES(?,?,?,?,?,?,?,?,?,?);";
+    Connection connection = null;
+    PreparedStatement insertStmt = null;
+    ResultSet resultKey = null;
+    try {
+      connection = getConnection();
+      insertStmt = connection.prepareStatement(insertDonation, Statement.RETURN_GENERATED_KEYS);
+      insertStmt.setString(1, donation.getCycle());
+      insertStmt.setString(2, donation.getFecRecNo());
+      insertStmt.setString(3, donation.getPacId());
+      insertStmt.setString(4, donation.getCid());
+      insertStmt.setFloat(5, donation.getAmount());
+      insertStmt.setDate(6, new java.sql.Date(donation.getDate().getTime()));
+      insertStmt.setString(7, donation.getRealCode());
+      insertStmt.setString(8, donation.getType());
+      insertStmt.setString(9, donation.getDi());
+      insertStmt.setString(10, donation.getFecCandId());
+      insertStmt.executeUpdate();
+
+      // Retrieve the auto-generated key and set it, so it can be used by the caller.
+      // For more details, see:
+      // http://dev.mysql.com/doc/connector-j/en/connector-j-usagenotes-last-insert-id.html
+      resultKey = insertStmt.getGeneratedKeys();
+      int recNo = -1;
+      if (resultKey.next()) {
+        recNo = resultKey.getInt(1);
+      } else {
+        throw new SQLException("Unable to retrieve auto-generated key.");
+      }
+      donation.setFecRecNo(Integer.toString(recNo));
+      return donation;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if (connection != null) {
+        connection.close();
+      }
+      if (insertStmt != null) {
+        insertStmt.close();
+      }
+      if (resultKey != null) {
+        resultKey.close();
+      }
+    }
   }
 
   /**
    * {@inheritDoc}
    */
   @Override
-  public List<PacToCandidateDonation> getDonationsByCandidate(Candidate candidate)
+  public List<PacToCandidateDonation> getDonationsByCandidateName(String candidateName)
       throws SQLException {
     List<PacToCandidateDonation> donationsToCandidate = new ArrayList<PacToCandidateDonation>();
-    String getDonationsToCandidate = "SELECT * FROM PACsToCand16 WHERE CID=?;";
+    String getDonationsToCandidate =
+        "SELECT * FROM PACsToCand16 AS ptc16 INNER JOIN CandsCRP16 AS c16 ON ptc16.CID=c16.CID WHERE c16.FirstLastP LIKE '%?%';";
     Connection connection = null;
     PreparedStatement selectStmt = null;
     ResultSet results = null;
     try {
       connection = getConnection();
       selectStmt = connection.prepareStatement(getDonationsToCandidate);
-      selectStmt.setString(1, candidate.getCid());
+      selectStmt.setString(1, candidateName);
       results = selectStmt.executeQuery();
       while (results.next()) {
         donationsToCandidate.add(parseDonation(results));
@@ -69,18 +115,62 @@ public class JdbcPacToCandidateDao extends MyJdbcDaoSupport implements PacToCand
    * {@inheritDoc}
    */
   @Override
-  public PacToCandidateDonation updateDonationDate(PacToCandidateDonation donation, Date date) {
-    // TODO Auto-generated method stub
-    return null;
+  public PacToCandidateDonation updateDonationDate(PacToCandidateDonation donation, Date date)
+      throws SQLException {
+    String updateDate = "UPDATE PACsToCand16 SET Date=? WHERE FECRecNo=?;";
+    Connection connection = null;
+    PreparedStatement updateStmt = null;
+    try {
+      connection = getConnection();
+      updateStmt = connection.prepareStatement(updateDate);
+      updateStmt.setDate(1, new java.sql.Date(date.getTime()));
+      updateStmt.setString(2, donation.getFecRecNo());
+      updateStmt.executeUpdate();
+
+      // Update the cycle param before returning to the caller.
+      donation.setDate(date);
+      return donation;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if (connection != null) {
+        connection.close();
+      }
+      if (updateStmt != null) {
+        updateStmt.close();
+      }
+    }
   }
 
   /**
    * {@inheritDoc}
    */
   @Override
-  public PacToCandidateDonation deleteDonation(PacToCandidateDonation donation) {
-    // TODO Auto-generated method stub
-    return null;
+  public PacToCandidateDonation deleteDonation(String fecRecNo) throws SQLException {
+    String deleteByFECCandId = "DELETE FROM PACsToCand16 WHERE FECRecNo=?;";
+    Connection connection = null;
+    PreparedStatement deleteStmt = null;
+    try {
+      connection = getConnection();
+      deleteStmt = connection.prepareStatement(deleteByFECCandId);
+      deleteStmt.setString(1, fecRecNo);
+      int affectedRows = deleteStmt.executeUpdate();
+      if (affectedRows == 0) {
+        throw new SQLException("No records available to delete for FECRecNo=" + fecRecNo);
+      }
+      return null;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if (connection != null) {
+        connection.close();
+      }
+      if (deleteStmt != null) {
+        deleteStmt.close();
+      }
+    }
   }
 
   /**
