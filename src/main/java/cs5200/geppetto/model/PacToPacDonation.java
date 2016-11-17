373da45bfe8e54800f21c4355930c73c4e9f16fe
Joshua Veden <veden.joshua@gmail.com>
2016-11-17 14:46:17 -0800

    added PacToPacDonation model

---
+++ src/main/java/cs5200/geppetto/model/PacToPacDonation.java
@@ -0,0 +1,398 @@
+package cs5200.geppetto.model;
+
+import java.util.Date;
+
+public class PacToPacDonation {
+  private String cycle;
+  private String fecRecNo;
+  private String filerId;
+  private String donorCmte;
+  private String contribLendTrans;
+  private String city;
+  private String state;
+  private String zip;
+  private String fecOccEmp;
+  private String primCode;
+  private Date date;
+  private Float amount;
+  private String recipId;
+  private String party;
+  private String otherId;
+  private String recipCode;
+  private String recipPrimCode;
+  private String amend;
+  private String report;
+  private String pg;
+  private String microfilm;
+  private String type;
+  private String realCode;
+  private String source;
+
+  public PacToPacDonation(String cycle, String fecRecNo, String filerId, String donorCmte,
+      String contribLendTrans, String city, String state, String zip, String fecOccEmp,
+      String primCode, Date date, Float amount, String recipId, String party, String otherId,
+      String recipCode, String recipPrimCode, String amend, String report, String pg,
+      String microfilm, String type, String realCode, String source) {
+    super();
+    this.cycle = cycle;
+    this.fecRecNo = fecRecNo;
+    this.filerId = filerId;
+    this.donorCmte = donorCmte;
+    this.contribLendTrans = contribLendTrans;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.fecOccEmp = fecOccEmp;
+    this.primCode = primCode;
+    this.date = date;
+    this.amount = amount;
+    this.recipId = recipId;
+    this.party = party;
+    this.otherId = otherId;
+    this.recipCode = recipCode;
+    this.recipPrimCode = recipPrimCode;
+    this.amend = amend;
+    this.report = report;
+    this.pg = pg;
+    this.microfilm = microfilm;
+    this.type = type;
+    this.realCode = realCode;
+    this.source = source;
+  }
+
+  /**
+   * @return the cycle
+   */
+  public String getCycle() {
+    return this.cycle;
+  }
+
+  /**
+   * @param cycle the cycle to set
+   */
+  public void setCycle(String cycle) {
+    this.cycle = cycle;
+  }
+
+  /**
+   * @return the fecRecNo
+   */
+  public String getFecRecNo() {
+    return this.fecRecNo;
+  }
+
+  /**
+   * @param fecRecNo the fecRecNo to set
+   */
+  public void setFecRecNo(String fecRecNo) {
+    this.fecRecNo = fecRecNo;
+  }
+
+  /**
+   * @return the filerId
+   */
+  public String getFilerId() {
+    return this.filerId;
+  }
+
+  /**
+   * @param filerId the filerId to set
+   */
+  public void setFilerId(String filerId) {
+    this.filerId = filerId;
+  }
+
+  /**
+   * @return the donorCmte
+   */
+  public String getDonorCmte() {
+    return this.donorCmte;
+  }
+
+  /**
+   * @param donorCmte the donorCmte to set
+   */
+  public void setDonorCmte(String donorCmte) {
+    this.donorCmte = donorCmte;
+  }
+
+  /**
+   * @return the contribLendTrans
+   */
+  public String getContribLendTrans() {
+    return this.contribLendTrans;
+  }
+
+  /**
+   * @param contribLendTrans the contribLendTrans to set
+   */
+  public void setContribLendTrans(String contribLendTrans) {
+    this.contribLendTrans = contribLendTrans;
+  }
+
+  /**
+   * @return the city
+   */
+  public String getCity() {
+    return this.city;
+  }
+
+  /**
+   * @param city the city to set
+   */
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  /**
+   * @return the state
+   */
+  public String getState() {
+    return this.state;
+  }
+
+  /**
+   * @param state the state to set
+   */
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  /**
+   * @return the zip
+   */
+  public String getZip() {
+    return this.zip;
+  }
+
+  /**
+   * @param zip the zip to set
+   */
+  public void setZip(String zip) {
+    this.zip = zip;
+  }
+
+  /**
+   * @return the fecOccEmp
+   */
+  public String getFecOccEmp() {
+    return this.fecOccEmp;
+  }
+
+  /**
+   * @param fecOccEmp the fecOccEmp to set
+   */
+  public void setFecOccEmp(String fecOccEmp) {
+    this.fecOccEmp = fecOccEmp;
+  }
+
+  /**
+   * @return the primCode
+   */
+  public String getPrimCode() {
+    return this.primCode;
+  }
+
+  /**
+   * @param primCode the primCode to set
+   */
+  public void setPrimCode(String primCode) {
+    this.primCode = primCode;
+  }
+
+  /**
+   * @return the date
+   */
+  public Date getDate() {
+    return this.date;
+  }
+
+  /**
+   * @param date the date to set
+   */
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  /**
+   * @return the amount
+   */
+  public Float getAmount() {
+    return this.amount;
+  }
+
+  /**
+   * @param amount the amount to set
+   */
+  public void setAmount(Float amount) {
+    this.amount = amount;
+  }
+
+  /**
+   * @return the recipId
+   */
+  public String getRecipId() {
+    return this.recipId;
+  }
+
+  /**
+   * @param recipId the recipId to set
+   */
+  public void setRecipId(String recipId) {
+    this.recipId = recipId;
+  }
+
+  /**
+   * @return the party
+   */
+  public String getParty() {
+    return this.party;
+  }
+
+  /**
+   * @param party the party to set
+   */
+  public void setParty(String party) {
+    this.party = party;
+  }
+
+  /**
+   * @return the otherId
+   */
+  public String getOtherId() {
+    return this.otherId;
+  }
+
+  /**
+   * @param otherId the otherId to set
+   */
+  public void setOtherId(String otherId) {
+    this.otherId = otherId;
+  }
+
+  /**
+   * @return the recipCode
+   */
+  public String getRecipCode() {
+    return this.recipCode;
+  }
+
+  /**
+   * @param recipCode the recipCode to set
+   */
+  public void setRecipCode(String recipCode) {
+    this.recipCode = recipCode;
+  }
+
+  /**
+   * @return the recipPrimCode
+   */
+  public String getRecipPrimCode() {
+    return this.recipPrimCode;
+  }
+
+  /**
+   * @param recipPrimCode the recipPrimCode to set
+   */
+  public void setRecipPrimCode(String recipPrimCode) {
+    this.recipPrimCode = recipPrimCode;
+  }
+
+  /**
+   * @return the amend
+   */
+  public String getAmend() {
+    return this.amend;
+  }
+
+  /**
+   * @param amend the amend to set
+   */
+  public void setAmend(String amend) {
+    this.amend = amend;
+  }
+
+  /**
+   * @return the report
+   */
+  public String getReport() {
+    return this.report;
+  }
+
+  /**
+   * @param report the report to set
+   */
+  public void setReport(String report) {
+    this.report = report;
+  }
+
+  /**
+   * @return the pg
+   */
+  public String getPg() {
+    return this.pg;
+  }
+
+  /**
+   * @param pg the pg to set
+   */
+  public void setPg(String pg) {
+    this.pg = pg;
+  }
+
+  /**
+   * @return the microfilm
+   */
+  public String getMicrofilm() {
+    return this.microfilm;
+  }
+
+  /**
+   * @param microfilm the microfilm to set
+   */
+  public void setMicrofilm(String microfilm) {
+    this.microfilm = microfilm;
+  }
+
+  /**
+   * @return the type
+   */
+  public String getType() {
+    return this.type;
+  }
+
+  /**
+   * @param type the type to set
+   */
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  /**
+   * @return the realCode
+   */
+  public String getRealCode() {
+    return this.realCode;
+  }
+
+  /**
+   * @param realCode the realCode to set
+   */
+  public void setRealCode(String realCode) {
+    this.realCode = realCode;
+  }
+
+  /**
+   * @return the source
+   */
+  public String getSource() {
+    return this.source;
+  }
+
+  /**
+   * @param source the source to set
+   */
+  public void setSource(String source) {
+    this.source = source;
+  }
+}
